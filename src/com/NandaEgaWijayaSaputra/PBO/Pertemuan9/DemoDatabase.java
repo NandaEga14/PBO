11688aebd4fad2a2d123881d9c9e63a94ca51365
Nandaega14 <21102195@ittelkom-pwt.ac.id>
2023-06-14 15:58:26 +0700

    first

---
+++ src/com/NandaEgaWijayaSaputra/PBO/Pertemuan9/DemoDatabase.java
@@ -0,0 +1,14 @@
+package com.NandaEgaWijayaSaputra.PBO.Pertemuan9;
+
+import javax.swing.*;
+
+public class DemoDatabase {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
+    private JTextField textNama;
+    private JTextField textNim;
+    private JSpinner sprNilai;
+    private JButton submitButton;
+    private JButton clearButton;
+    private JTable dataTable;
+}
